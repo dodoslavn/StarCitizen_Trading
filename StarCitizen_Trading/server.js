d232f14b6756608f4e2a77bb499f91c21579c56f
Dodoslav Novak <34769717+dodoslavn@users.noreply.github.com>
2025-02-08 02:56:12 +0100

    Update server.js

--- StarCitizen_Trading/server.js
+++ StarCitizen_Trading/server.js
@@ -1,13 +1,17 @@
-console.log("INFO: Node.js version - ", process.version);
+console.log("INFO: Node.js version -", process.version);
 
 const http_server = require('http');
+const cron = require('node-cron');
 const functions = require('./functions.js');
+
+global.config = functions.loadConfig();
 
 global.cachedData = null;
 global.profit = [];
 
 functions.refreshData();
+cron.schedule('* * * * *', functions.refreshData );
 
 const my_server = http_server.createServer(functions.processRequest);
 
-my_server.listen(3000, '0.0.0.0', functions.serverStarted());
+my_server.listen(config.http_server.port_listen, config.http_server.iplisten, functions.serverStarted());
